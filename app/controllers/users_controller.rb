c249800eb7c3032782a09bfffff06c7bda6519ed
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-02 13:36:23 -0400

    user controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,15 +27,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
   private
 
     # Only allow a list of trusted parameters through.
