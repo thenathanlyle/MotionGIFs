1842d4f8a1c3384a3615f3e619aed7f194979829
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-02 16:49:53 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :posts
   post '/auth/login', to: 'authentications#login'
   get '/auth/verify', to: 'authentications#verify'
-  resources :users
+  resources :users, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
