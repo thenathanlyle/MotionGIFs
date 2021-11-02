903b894bbaf5d952114e0d862bc173433e881121
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-02 14:33:30 -0400

    backend working on postman

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Post.destroy_all
+Comment.destroy_all
 
 @admin = User.create!(username: 'hummingbirds', email: 'hummingbirds@email.com', image_url: 'test.jpg', password: '123456')
 
 puts "#{User.count} users created!"
+
+
+@meme = Post.create!(image_url: 'test.jpg', tag: '#lego', user: @admin)
+
+puts "#{Post.count} posts created!"
+
+Comment.create!(opinion: 'I love this show', user: @admin, post: @meme)
+
+puts "#{Comment.count} comments created!"
