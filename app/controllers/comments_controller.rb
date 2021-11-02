bcc255e97b19d3ea93f76a7349c214f96afc9d4c
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-02 13:53:37 -0400

    models logic

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,10 +8,10 @@ class CommentsController < ApplicationController
     render json: @comments
   end
 
-  # GET /comments/1
-  def show
-    render json: @comment
-  end
+  # # GET /comments/1
+  # def show
+  #   render json: @comment
+  # end
 
   # POST /comments
   def create
@@ -24,25 +24,25 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  def update
-    if @comment.update(comment_params)
-      render json: @comment
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /comments/1
+  # def update
+  #   if @comment.update(comment_params)
+  #     render json: @comment
+  #   else
+  #     render json: @comment.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /comments/1
-  def destroy
-    @comment.destroy
-  end
+  # # DELETE /comments/1
+  # def destroy
+  #   @comment.destroy
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_comment
+    #   @comment = Comment.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     def comment_params
