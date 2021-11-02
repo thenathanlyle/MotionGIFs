f59884cc2a6a6a0bea77346a1dea4d38dca4c5d5
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-02 13:40:32 -0400

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+@admin = User.create!(username: 'hummingbirds', email: 'hummingbirds@email.com', image_url: 'test.jpg', password: '123456')
+
+puts "#{User.count} users created!"
