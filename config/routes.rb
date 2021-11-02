955260cdfb99639595f6fa866155669e0b6bd5ea
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-02 14:15:57 -0400

    fixed routes and started postman

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  post '/auth/login', to: 'authentication#login'
-  get '/auth/verify', to: 'authentication#verify'
   resources :comments
   resources :posts
+  post '/auth/login', to: 'authentications#login'
+  get '/auth/verify', to: 'authentications#verify'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
