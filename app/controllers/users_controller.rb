54a994cdf9cd0eab7d40cc2a506721864d5e9e72
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-02 12:31:20 -0400

    added auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
   def index
@@ -18,7 +17,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode({ id: @user.id })
+      render json: {
+        user: @user.attributes.except('password_digest'),
+        token: @token
+      }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -33,16 +36,7 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
     # Only allow a list of trusted parameters through.
     def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :image_url, presence: true
+  validates :password, length: { minimum: 6 }
 end
