45b0c3217332918794ba532d3448f44cba3e27ab
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-04 14:52:11 -0400

    fixed postcard

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -10,23 +10,27 @@ export default function PostCard({ currentUser, post, handlePostDelete }) {
     <div className="post-card">
       <img src={post?.image_url} alt="profile-pic" />
       <h3>{post?.username}</h3>
-      {/* {currentUser?.id &&
-        (<>
-          <img src={post?.image_url} alt="meme-pic" />
-          {currentUser && currentUser.id === post?.user_id ? (
-            <>
-              <Link to={`/posts/${post.id}/update`}>Edit</Link>
-              <button onClick={() => handlePostDelete(post.id)}>Delete</button>
-            </>
-          ) : null}
-        </>)(
+      {currentUser && (
+        <>
+          <>
+            <img src={post?.image_url} alt="meme-pic" />
+            {currentUser && currentUser.id === post?.user_id ? (
+              <>
+                <Link to={`/posts/${post.id}/update`}>Edit</Link>
+                <button onClick={() => handlePostDelete(post.id)}>
+                  Delete
+                </button>
+              </>
+            ) : null}
+          </>
           <>
             <h5>Comments: {post.comments.length}</h5>
             <img src={commentLogo} alt="MotionGIFs-comment"></img> <h5></h5>
-          {post.comments.length}
+            {post.comments.length}
             <Link to={`/posts/${post.id}`}>View Meme</Link>
           </>
-        )} */}
+        </>
+      )}
     </div>
   );
 }
