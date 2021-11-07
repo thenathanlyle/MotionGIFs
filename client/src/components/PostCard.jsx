cf118dcee320fd8d28d012395cc671e5a2c530e7
Nathan Lyle <brickultra@Nathans-Mac-mini.local>
2021-11-07 00:34:08 -0400

    postcard

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -8,6 +8,7 @@ import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import Badge from "@mui/material/Badge";
 import ChatIcon from "@mui/icons-material/Chat";
 import { createTheme } from "@mui/material/styles";
+import user from "../assets/user.png";
 
 export default function PostCard(props) {
   const { currentUser, post, handlePostDelete } = props;
@@ -36,44 +37,41 @@ export default function PostCard(props) {
   return (
     <div className="post-card">
       <div className="user-post-card">
-        <img
-          className="user-image"
-          src={post?.user?.image_url}
-          alt="profile-pic"
-        />
+        <img className="user-image" src={user} alt="profile-pic" />
         <h3 className="user-name">{post?.user?.username}</h3>
+
+        {currentUser && currentUser.id === post?.user_id ? (
+          <div className="post-dots">
+            <IconButton
+              id="basic-button"
+              theme={theme}
+              color="secondary"
+              aria-controls="basic-menu"
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              <MoreHorizIcon />
+            </IconButton>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem component={Link} to={`/posts/${post.id}/update`}>
+                Edit
+              </MenuItem>
+              <MenuItem onClick={() => handlePostDelete(post.id)}>
+                Delete
+              </MenuItem>
+            </Menu>
+          </div>
+        ) : null}
       </div>
-      {currentUser && currentUser.id === post?.user_id ? (
-        <div className="post-card-edit-delete">
-          <IconButton
-            id="basic-button"
-            theme={theme}
-            color="secondary"
-            aria-controls="basic-menu"
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-          >
-            <MoreHorizIcon />
-          </IconButton>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem component={Link} to={`/posts/${post.id}/update`}>
-              Edit
-            </MenuItem>
-            <MenuItem onClick={() => handlePostDelete(post.id)}>
-              Delete
-            </MenuItem>
-          </Menu>
-        </div>
-      ) : null}
       <Link to={`/posts/${post.id}/`}>
         <img className="post-image" src={post?.image_url} alt="meme-pic" />
       </Link>
